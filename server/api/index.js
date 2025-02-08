c1aaf222d4c9c2c4ad7636e8b7afdde00c2db35b
Kshitiz Jain <kshitizj41@gmail.com>
2025-02-09 03:12:17 +0530

    index.js changes

--- server/api/index.js
+++ server/api/index.js
@@ -54,4 +54,9 @@ app.get("/api/data", async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
